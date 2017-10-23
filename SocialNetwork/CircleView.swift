ce9e7d211fb829f7c0b747566a5ee1cf2874cd4b
Ricardo Lousada <ricardo.lousada@gmail.com>
2017-10-23 19:54:34 +0100

    feed UI done

--- SocialNetwork/CircleView.swift
+++ SocialNetwork/CircleView.swift
@@ -1,15 +1,15 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialNetwork
 //
-//  Created by Ricardo Lousada on 22/10/17.
+//  Created by Ricardo Lousada on 23/10/17.
 //  Copyright © 2017 IT-4-ALL. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,10 +18,19 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
-        
-        
+
     }
+    
+
+    override func layoutSubviews() {
+    
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width/2
+    
+    
+    
+    }
+    
+    
     
 }
